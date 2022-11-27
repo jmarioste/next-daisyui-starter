0f9b78d7ae90579a4ac1758e21fbe3e261037f7a
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-11-27 21:14:44 +0800

    use getToken

--- middleware.ts
+++ middleware.ts
@@ -1,54 +1,26 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import withAuth, { NextRequestWithAuth } from "next-auth/middleware";
-import { NextMiddlewareResult } from "next/dist/server/web/types";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
 
-const requiresLogin = ["/admin"];
-// This function can be marked `async` if using `await` inside
+//paths that require authentication / authorization
+const requireAuth: string[] = ["/admin"];
+
 export async function middleware(request: NextRequest) {
   const res = NextResponse.next();
   const pathname = request.nextUrl.pathname;
 
-  // if (requiresLogin.some((path) => pathname.startsWith(path))) {
-  //   // return withAuth(request as NextRequestWithAuth);
-  //   const session = await getToken({ req: request });
-  //   if (!session) {
-  //     const url = new URL(`/api/auth/signin`, request.url);
-  //     url.searchParams.set("redirectUrl", encodeURI(request.url));
-  //     return NextResponse.redirect(url);
-  //   }
-  // }
+  if (requireAuth.some((path) => pathname.startsWith(path))) {
+    const token = await getToken({ req: request });
+    //check not logged in
+    if (!token) {
+      const url = new URL(`/api/auth/signin`, request.url);
+      url.searchParams.set("redirectUrl", encodeURI(request.url));
+      return NextResponse.redirect(url);
+    }
 
-  if (requiresLogin.some((path) => pathname.startsWith(path))) {
-    try {
-      const result = (await withAuth(request as NextRequestWithAuth, {
-        callbacks: {
-          authorized: ({ token }) => {
-            const isAdmin = token?.role === "admin";
-            if (!token) {
-              throw new Error("401");
-            }
-            if (!isAdmin) {
-              throw Error("403");
-            }
-            return isAdmin;
-          },
-        },
-      })) as NextMiddlewareResult;
-      return result;
-    } catch (error) {
-      if (error instanceof Error) {
-        if (error.message === "403") {
-          const url = new URL("/403", request.url);
-          return NextResponse.rewrite(url);
-        }
-
-        if (error.message === "401") {
-          const url = new URL("/signin", request.url);
-          url.searchParams.set("callbackUrl", encodeURI(request.url));
-          return NextResponse.redirect(url);
-        }
-      }
+    //check if not authorized
+    if (token.role !== "admin") {
+      const url = new URL(`/403`, request.url);
+      return NextResponse.rewrite(url);
     }
   }
   return res;
